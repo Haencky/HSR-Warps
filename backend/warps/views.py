8979b58d8ae044bd48bbdfedf5f45df739b702f5
Haencky <haenck@proton.me>
2026-01-08 22:37:53 +0100

    ordered items by name

--- backend/warps/views.py
+++ backend/warps/views.py
@@ -130,5 +130,5 @@ def item_types(request):
 
 @api_view(['GET'])
 def warps_per_item(request):
-    warps_per_item = Warp.objects.all().values('item_id').annotate(item_image=F('item_id__image'), count=Count('item_id'), item_name=F('item_id__name'), item_type=F('item_id__typ__name'), item_rarity=F('item_id__rarity'))
+    warps_per_item = Warp.objects.all().values('item_id').annotate(item_image=F('item_id__image'), count=Count('item_id'), item_name=F('item_id__name'), item_type=F('item_id__typ__name'), item_rarity=F('item_id__rarity')).order_by('item_name')
     return Response(WarpsPerItemSerializer(warps_per_item, many=True, context={'request': request}).data)
